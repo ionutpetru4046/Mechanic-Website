f1995b14a55875ede8e21e9fd374737cfa481f9f
ionutpetru4046 <ionut4046@yahoo.com>
2025-07-19 13:57:16 +0100

    Fix CORS with allowed origins for frontend

--- backend/server.js
+++ backend/server.js
@@ -9,26 +9,38 @@ import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors()); // for parsing JSON bodies
+
+const allowedOrigins = [
+  'https://mechanic-website-tau.vercel.app',
+  'http://localhost:3000',
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `CORS policy: Access denied from origin ${origin}`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  }),
+);
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 // Routes
 app.use('/api/auth', authRoutes);
-
-// users api
 app.use('/api/users', userRoutes);
-
-// booking routes
 app.use('/api/bookings', bookingRouter);
 
-// Test route to check server is working
 app.get('/', (req, res) => {
   res.send('API is running 🟢');
 });
 
-// Connect to MongoDB and start server
 mongoose
   .connect(MONGO_URI)
   .then(() => {
